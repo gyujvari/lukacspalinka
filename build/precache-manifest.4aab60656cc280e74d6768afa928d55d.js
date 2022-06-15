bdd4e11cf9d660c056b959236bdff30d4ab11cbe
gyorgy <ujvari.gyorgy@gmail.com>
2022-06-15 13:51:19 +0300

    push project

---
+++ build/precache-manifest.4aab60656cc280e74d6768afa928d55d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22dfd51cbc2889ffcdb1426a9fe78389",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0149f4de62ab5251010",
+    "url": "/static/css/main.cf23778c.chunk.css"
+  },
+  {
+    "revision": "191274e425820b21443d",
+    "url": "/static/js/2.838fa1b5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.838fa1b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0149f4de62ab5251010",
+    "url": "/static/js/main.d81aa84a.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
